4d2bb48e4f1bec89879b9a0b4bc90497aa34cfb3
Reis <120048234+ReisWebDev@users.noreply.github.com>
2023-07-09 02:02:48 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author '</Reis>'
+description 'PPNotify'
+version '5.4'
+
+ui_page 'html/ui.html'
+
+client_scripts {
+	'client.lua',
+}
+
+files {
+	'html/*.*'
+}
+
+export 'Alert'
